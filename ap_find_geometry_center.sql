1ba2c2e5b471ea24b128338706d5873a7a234cd9
Ahmad Payan <30901944+AhmadPayan@users.noreply.github.com>
2018-03-21 10:41:44 +0330

    Update ap_find_geometry_center.sql

--- ap_find_geometry_center.sql
+++ ap_find_geometry_center.sql
@@ -3,26 +3,24 @@ CREATE OR REPLACE FUNCTION public.ap_find_geometry_center(geometry geometry)
 $BODY$
 
 DECLARE
-
 geometryType text;
 centerOfGeometry geometry;
 
 BEGIN
 
--- Type of geometry : Line,Point,Polygon
-
+-- Types of geometry : Line,Point,Polygon,...
 geometryType := (SELECT ST_GeometryType(geometry));
 
 IF (geometryType = 'ST_LineString') THEN
-centerOfGeometry := (select ST_LineInterpolatePoint(geometry,0.5));
+  centerOfGeometry := (select ST_LineInterpolatePoint(geometry,0.5));
 ELSIF (geometryType = 'ST_MultiLineString') THEN
--- It'll find the nearest point to the MultiLineString
-centerOfGeometry := (select ST_ClosestPoint(geometry,ST_Centroid(geometry)));
+  -- It'll find the nearest point to the MultiLineString
+  centerOfGeometry := (select ST_ClosestPoint(geometry,ST_Centroid(geometry)));
 ELSIF (geometryType  = 'ST_Point') THEN
--- There is no need to do any further processing for point geometry, just return itself
-centerOfGeometry := geometry;
+  -- There is no need to do any further processing for point geometry, just return itself
+  centerOfGeometry := geometry;
 ELSE
-centerOfGeometry := (SELECT ST_Centroid(geometry));
+  centerOfGeometry := (SELECT ST_Centroid(geometry));
 END IF;
 
 RETURN QUERY SELECT centerOfGeometry;
